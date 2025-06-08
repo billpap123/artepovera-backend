d83e3747aa87beef330647feeedfb838b21b5aee
Vasilis Papazafeiropoulos <vpapazafeiropoulos@gmail.com>
2025-06-08 21:50:31 +0300

    Some commit message

--- backend/src/models/associations.ts
+++ backend/src/models/associations.ts
@@ -9,62 +9,54 @@ import Review from './Review';
 import Portfolio from './Portfolio';
 import JobApplication from './JobApplication';
 import ArtistSupport from './ArtistSupport';
-import ArtistComment from './ArtistComment'; // <<< ADD THIS IMPORT
+import ArtistComment from './ArtistComment';
 
 // 1) User <-> Artist Profile
-User.hasOne(Artist, { foreignKey: 'user_id', as: 'artistProfile' });
-Artist.belongsTo(User, { foreignKey: 'user_id', as: 'user' }); // <<< CORRECTED ALIAS to 'user'
+User.hasOne(Artist, {
+  foreignKey: 'user_id',
+  as: 'artistProfile',
+  onDelete: 'CASCADE' // <<< ADD THIS: Deletes Artist profile when User is deleted
+});
+Artist.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 
 // 2) User <-> Employer Profile
-User.hasOne(Employer, { foreignKey: 'user_id', as: 'employerProfile' });
-Employer.belongsTo(User, { foreignKey: 'user_id', as: 'user' }); // This alias is correct
+User.hasOne(Employer, {
+  foreignKey: 'user_id',
+  as: 'employerProfile',
+  onDelete: 'CASCADE' // <<< ADD THIS: Deletes Employer profile when User is deleted
+});
+Employer.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 
 // 3) Chat <-> Artist & Chat <-> Employer
-// Chat model's 'artist_user_id' attribute maps to DB 'artist_id' (PK of Artist table)
-// Chat model's 'employer_user_id' attribute maps to DB 'employer_id' (PK of Employer table)
+// These associations are from Chat to Artist/Employer, not directly to User.
+// The deletion of Artist/Employer profiles (handled by the above) should cascade to these.
+Chat.belongsTo(Artist, { foreignKey: 'artist_user_id', targetKey: 'artist_id', as: 'chatArtistProfile' });
+Artist.hasMany(Chat, { foreignKey: 'artist_user_id', sourceKey: 'artist_id' });
 
-Chat.belongsTo(Artist, {
-  foreignKey: 'artist_user_id',    // This is the attribute name in the Chat model
-  targetKey: 'artist_id',         // This is the PK attribute name in the Artist model
-  as: 'chatArtistProfile'         // <<< Alias used in Chat.findAll includes
-});
-Artist.hasMany(Chat, {
-  foreignKey: 'artist_user_id',    // Attribute name in Chat model that links to Artist
-  sourceKey: 'artist_id',          // PK attribute in Artist model
-  // as: 'artistChats' // Optional alias for reverse if you need to fetch chats from an Artist instance
-});
-
-Chat.belongsTo(Employer, {
-  foreignKey: 'employer_user_id',  // Attribute name in Chat model
-  targetKey: 'employer_id',      // PK attribute in Employer model
-  as: 'chatEmployerProfile'      // <<< Alias used in Chat.findAll includes
-});
-Employer.hasMany(Chat, {
-  foreignKey: 'employer_user_id',  // Attribute name in Chat model
-  sourceKey: 'employer_id',        // PK attribute in Employer model
-  // as: 'employerChats' // Optional alias
-});
-
-// User.hasMany(Chat, ...) for artist_user_id and employer_user_id are removed
-// because those fields in Chat now reference Artist/Employer PKs, not User PKs.
-// Fetching chats for a specific user is handled by the logic in chatController.fetchMessages.
+Chat.belongsTo(Employer, { foreignKey: 'employer_user_id', targetKey: 'employer_id', as: 'chatEmployerProfile' });
+Employer.hasMany(Chat, { foreignKey: 'employer_user_id', sourceKey: 'employer_id' });
 
 // 4) Chat <-> Message
-Chat.hasMany(Message, { foreignKey: 'chat_id', as: 'chatMessages' });
-Message.belongsTo(Chat, { foreignKey: 'chat_id', as: 'chat' }); // Using 'chat' for symmetry
+Chat.hasMany(Message, {
+  foreignKey: 'chat_id',
+  as: 'chatMessages',
+  onDelete: 'CASCADE' // Deletes messages if a chat is deleted
+});
+Message.belongsTo(Chat, { foreignKey: 'chat_id', as: 'chat' });
 
 // 5) Message <-> User (for sender and receiver)
 Message.belongsTo(User, { foreignKey: 'sender_id', as: 'messageSender' });
 Message.belongsTo(User, { foreignKey: 'receiver_id', as: 'messageReceiver' });
+// Add the corresponding hasMany with cascade
+User.hasMany(Message, { foreignKey: 'sender_id', as: 'sentMessages', onDelete: 'CASCADE' });
+User.hasMany(Message, { foreignKey: 'receiver_id', as: 'receivedMessages', onDelete: 'CASCADE' });
 
 // 6) JobPosting <-> Employer
-JobPosting.belongsTo(Employer, {
-  foreignKey: 'employer_id',
-  as: 'employer'
-});
+JobPosting.belongsTo(Employer, { foreignKey: 'employer_id', as: 'employer' });
 Employer.hasMany(JobPosting, {
   foreignKey: 'employer_id',
-  as: 'jobPostings'
+  as: 'jobPostings',
+  onDelete: 'CASCADE' // Deletes job postings if an employer is deleted
 });
 
 // 7) Review <-> Chat & Review <-> User
@@ -72,71 +64,65 @@ Review.belongsTo(Chat, { foreignKey: 'chat_id', as: 'chat' });
 Chat.hasMany(Review, { foreignKey: 'chat_id', as: 'reviews' });
 
 Review.belongsTo(User, { foreignKey: 'reviewer_user_id', as: 'reviewer' });
-User.hasMany(Review, { foreignKey: 'reviewer_user_id', as: 'reviewsGiven' });
+User.hasMany(Review, {
+  foreignKey: 'reviewer_user_id',
+  as: 'reviewsGiven',
+  onDelete: 'CASCADE' // <<< ADD THIS: Deletes reviews a user has written
+});
 
 Review.belongsTo(User, { foreignKey: 'reviewed_user_id', as: 'reviewed' });
-User.hasMany(Review, { foreignKey: 'reviewed_user_id', as: 'reviewsReceived' });
+User.hasMany(Review, {
+  foreignKey: 'reviewed_user_id',
+  as: 'reviewsReceived',
+  onDelete: 'CASCADE' // <<< ADD THIS: Deletes reviews a user has received
+});
 
 // Portfolio Associations
-Portfolio.belongsTo(Artist, {
-  foreignKey: 'artist_id',
-  targetKey: 'artist_id',
-  as: 'artist'
-});
+Portfolio.belongsTo(Artist, { foreignKey: 'artist_id', targetKey: 'artist_id', as: 'artist' });
 Artist.hasMany(Portfolio, {
   foreignKey: 'artist_id',
   sourceKey: 'artist_id',
-  as: 'portfolioItems'
+  as: 'portfolioItems',
+  onDelete: 'CASCADE' // Deletes portfolio items if an artist is deleted
 });
 
 // Job Application Associations
-JobPosting.hasMany(JobApplication, { foreignKey: 'job_id', as: 'applications' });
+JobPosting.hasMany(JobApplication, { foreignKey: 'job_id', as: 'applications', onDelete: 'CASCADE' });
 JobApplication.belongsTo(JobPosting, { foreignKey: 'job_id', as: 'jobPostingDetails' });
 
-User.hasMany(JobApplication, { foreignKey: 'artist_user_id', as: 'jobApplicationsMade' });
+User.hasMany(JobApplication, {
+  foreignKey: 'artist_user_id',
+  as: 'jobApplicationsMade',
+  onDelete: 'CASCADE' // <<< ADD THIS: Deletes applications a user has made
+});
 JobApplication.belongsTo(User, { foreignKey: 'artist_user_id', as: 'applyingArtistDetails' });
-// User (as supporter) -> ArtistSupport
+
+// Artist Support Associations
 User.hasMany(ArtistSupport, {
   foreignKey: 'supporter_artist_user_id',
-  as: 'givenSupports'
+  as: 'givenSupports',
+  onDelete: 'CASCADE' // <<< ADD THIS
 });
-ArtistSupport.belongsTo(User, {
-  foreignKey: 'supporter_artist_user_id',
-  as: 'supporterArtist'
-});
+ArtistSupport.belongsTo(User, { foreignKey: 'supporter_artist_user_id', as: 'supporterArtist' });
 
-// User (as supported) -> ArtistSupport
 User.hasMany(ArtistSupport, {
   foreignKey: 'supported_artist_user_id',
-  as: 'receivedSupports'
+  as: 'receivedSupports',
+  onDelete: 'CASCADE' // <<< ADD THIS
 });
-ArtistSupport.belongsTo(User, {
-  foreignKey: 'supported_artist_user_id',
-  as: 'supportedArtist'
-});
-// Make sure this file is imported ONCE in your application (e.g., in server.ts)
-// AFTER all models are defined and initialized by Sequelize.
-// Example: import './models/associations';
+ArtistSupport.belongsTo(User, { foreignKey: 'supported_artist_user_id', as: 'supportedArtist' });
 
-
-// --- ADD NEW ASSOCIATIONS FOR ARTIST PROFILE COMMENTS ---
-
-// An ArtistComment belongs to the User whose profile is being commented on.
-ArtistComment.belongsTo(User, {
+// Artist Profile Comment Associations
+ArtistComment.belongsTo(User, { foreignKey: 'profile_user_id', as: 'commentedProfileUser' });
+User.hasMany(ArtistComment, {
   foreignKey: 'profile_user_id',
-  as: 'commentedProfileUser' // Alias to get the User object of the profile owner
-});
-User.hasMany(ArtistComment, { // A User's profile can receive many comments
-  foreignKey: 'profile_user_id',
-  as: 'receivedProfileComments'
+  as: 'receivedProfileComments',
+  onDelete: 'CASCADE' // <<< ADD THIS
 });
 
-// An ArtistComment belongs to the User (Artist) who wrote the comment.
-ArtistComment.belongsTo(User, {
+ArtistComment.belongsTo(User, { foreignKey: 'commenter_user_id', as: 'commenterArtist' });
+User.hasMany(ArtistComment, {
   foreignKey: 'commenter_user_id',
-  as: 'commenterArtist' // Alias to get the User object of the commenter
-});
-User.hasMany(ArtistComment, { // A User (Artist) can write many comments
-  foreignKey: 'commenter_user_id',
-  as: 'writtenProfileComments'
+  as: 'writtenProfileComments',
+  onDelete: 'CASCADE' // <<< ADD THIS
 });
